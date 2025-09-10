c7d22daa3c9b84a7163a5a460b8addab04725053
harshal <harshalvankudre@gmail.com>
2025-09-10 13:15:22 +0530

    Refactor active session management by centralizing `activeSessions` in a dedicated server module. Update `login` and `logout` actions to use the shared module.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
+    import { injectAnalytics } from '@vercel/analytics/sveltekit'
+        import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
 
 	let { children } = $props();
+        injectSpeedInsights();
+
 </script>
 
 <svelte:head>
